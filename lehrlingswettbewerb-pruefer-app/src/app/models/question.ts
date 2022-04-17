5b3fc18918f85182027b6abc1f05c8b7b3e013f5
lorenzgruber <regru.lg@gmail.com>
2022-04-17 02:18:33 +0200

    remove unnecessary files

--- lehrlingswettbewerb-pruefer-app/src/app/models/question.ts
+++
@@ -1,6 +0,0 @@
-export interface Question {
-    questionID: string,
-    question: string,
-    answers: string[]
-    correct: 0 | 1 | 2
-}
--- lehrlingswettbewerb-pruefer-app/src/app/models/questionAnswer.ts
+++
@@ -1,6 +0,0 @@
-import { Question } from "./question";
-
-export interface QuestionAnswer {
-    question: Question,
-    answer: number
-}
